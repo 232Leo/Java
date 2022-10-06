0beeb94e870648c895dc42f1b5dccecc6122fdbe
LeoM12 <84456221+232Leo@users.noreply.github.com>
2022-10-06 15:53:15 +0000

    finished ch3.

--- script.js
+++ script.js
@@ -237,3 +237,91 @@
 
 
 //if statements
+// const age = 25;
+
+// if(age > 20){
+//   console.log('you are over 20 years old')
+// }
+
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi']
+
+// if(ninjas.length > 3){
+//   console.log("that's a lot of ninjas")
+// }
+
+// const password = 'p@ss';
+
+// if(password.length >=12 && password.includes('@')){
+//   console.log('that password is might strong');
+// }else if(password.length >= 8 || password.includes('@') && password.length > 5){
+//   console.log('that password is strong enough!');
+// }else{
+//   console.log('password is not strong enough');
+// }
+
+//logical NOT (!)
+
+// let user = false;
+
+// if(!user){
+//   console.log('you must be longed in to continue');
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+
+// break and continue
+
+// const scores = [10, 25, 0, 30, 100, 20, 10];
+
+// for(let i = 0; i < scores.length; i++){
+
+//   if(scores[i] === 0){
+//     continue;
+//   }
+  
+//   console.log('your score: ', scores[i]);
+  
+//   if(scores[i] == 100){
+//     console.log('congrats, you got the top score!');
+//     break;
+//   }
+// }
+
+// switch statements
+// const grade = 'D';
+
+// switch(grade){
+//   case 'A':
+//     console.log('you got an A!');
+//     break;
+//   case'B':
+//     console.log('you got an B!');
+//     break;
+//   case'C':
+//     console.log('you got an C!');
+//     break;
+//   case'D':
+//     console.log('you got an D!');
+//     break;
+//   case'E':
+//     console.log('you got an E!');
+//     break;
+//   default:
+//     console.log('not a valid grade');
+// }
+
+
+// variables & black scope
+
+if(true){
+  
+}
+
+
+
+
+
+
