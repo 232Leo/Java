2a5d7565185333c7caa9e9e1d6b83932ded7641c
LeoM12 <84456221+232Leo@users.noreply.github.com>
2022-10-10 19:49:02 +0000

    finshed

--- script.js
+++ script.js
@@ -484,17 +484,17 @@
 // user['name'] = 'chuni-li';
 // console.log(user['name']);
 
-//console.log(typeof user);
+// console.log(typeof user);
 
 
-//object literals
+// object literals
 
 // const blogs = [
 //   { title: 'why mac & cheese rules', likes: 30},
 //   { title:'10 things to make with marnite', likes: 50 }
 // ];
 
-//console.log(blogs);
+// console.log(blogs);
 
 // let user = {
 //   name: 'crystal',
@@ -546,8 +546,128 @@
 // console.log(Math.round(random * 100));
 
 
-// 
+//  primitive values
+
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100;
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// reference vlaues
+
+// const userOne = { name: 'ryu', age: 30 };
+// const userTwo = userOne;
+
+// console.log(userOne, userTwo); 
+
+// userOne.age = 40;
+// console.log(userOne, userTwo);
+
+
+// Chapter 6
+
+// const para = document.querySelector('body > h1');
+
+// console.log(para);
+
+
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
+
+// // paras.forEach(para => {
+// //   console.log(para);
+// // });
+
+// console.log(errors);
 
 
 
- 
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// // get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors[0]);
+
+
+// get elemets by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[0]);
+
+
+//const para = document.querySelector('p');
+
+// console.log(para.innerText);
+//para.innerText += ' ninjas are awesome!';
+
+//const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//   console.log(para.innerText);
+//   para.innerText += ' !new text';
+// });
+
+//const content = document.querySelector('.content');
+
+//console.log(content.innerHTML);
+
+//content.innerHTML += '<h2> this is a new h2</h2>';
+
+
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person => {
+//   content.innerHTML += `<p>${person}</p>`;
+// });
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// link.innerText = 'the net ninja website';
+
+// const mssg = document.querySelector('p');
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+// mssg.setAttribute('style', 'color: green;');
+
+// const title = document.querySelector('h1');
+
+// // title.setAttribute('style', 'margin; 50px;')
+
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = '60px';
+// title.style.margin = '';
+
+// const content = document.querySelector('p');
+
+// console.log(content.classList);
+// content.classList.add('error');
+// //content.classList.remove('error');
+// content.classList.add('success');
+
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+  if(p.textContent.includes('error')){
+    p.classList.add('error');
+  }
+  if(p.textContent.includes('success')){
+    p.classList.add('success');
+  }
+});
+
+const title = document.querySelector('.title');
+
+title.classList.toggle('test');
+title.classList.toggle('test');
+
+
